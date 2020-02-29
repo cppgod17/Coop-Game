108ce7dbba454e9741c0245fb4bde01dd8a01eaa
CppGod <viper_0_1@mail.ru>
2020-02-29 22:05:54 +0300

    add simple movement to the bot

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -3,6 +3,12 @@
 
 #include "STrackerBot.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Public/NavigationPath.h"
+#include "Kismet/GameplayStatics.h"
+#include "Public/NavigationSystem.h"
+#include "GameFramework/Character.h"
+#include "DrawDebugHelpers.h"
+#include "Public/NavigationPath.h"
 
 // Sets default values
 ASTrackerBot::ASTrackerBot()
@@ -13,15 +19,33 @@ ASTrackerBot::ASTrackerBot()
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	MeshComp->SetCanEverAffectNavigation(false);
 	RootComponent = MeshComp;
+	MeshComp->SetSimulatePhysics(true);
 	
-	
+	BUseVelocityChange = false;
+	MovementForce = 1000;
+	RequiredDistanceToTarget = 100.f;
 }
 
 // Called when the game starts or when spawned
 void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
+
+	NextPathPoint = GetNextPathPoint();
 	
+}
+
+FVector ASTrackerBot::GetNextPathPoint()
+{
+	ACharacter* PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+
+	if (NavPath->PathPoints.Num() > 1)
+	{
+		return NavPath->PathPoints[1];
+	}
+	return GetActorLocation();
+	DrawDebugString(GetWorld(), GetActorLocation(), "DUDUDSH!");
 }
 
 // Called every frame
@@ -29,6 +53,25 @@ void ASTrackerBot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+	if (DistanceToTarget <= RequiredDistanceToTarget)
+	{
+		NextPathPoint = GetNextPathPoint();
+
+		DrawDebugString(GetWorld(), GetActorLocation(), "Target Reached!");
+	}
+	else
+	{
+		FVector ForceDirection = NextPathPoint - GetActorLocation();
+		ForceDirection.Normalize();
+		ForceDirection *= MovementForce;
+
+		MeshComp->AddForce(ForceDirection, NAME_None, BUseVelocityChange);
+
+		DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), NextPathPoint, 30.f, FColor::Green, false, 0.f, 0, 1.f);
+	}
+	DrawDebugSphere(GetWorld(), NextPathPoint, 30.f, 12, FColor::Green, false, 4.f, 1.f);
 }
 
 
